1ffe2c76c55d3fbc0f2d6e0f7e6943aff7dfa221
廿陵 <nianlingbeige@163.com>
2025-05-13 22:14:16 +0800

    角色死亡复活，通知

---
+++ dnf/mail_config.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+
+__author__ = "廿陵 <wemean66@gmail.com> (GitHub: @nianling)"
+__version__ = '1.0'
+
+# 邮件配置
+config = {
+    # 发件人信息
+    "sender": "1293249206@qq.com",  # 发件人邮箱
+    "password": "hhxowfdjhkurjibc",  # 授权码
+
+    # SMTP 设置 默认QQ邮箱
+    "smtp_server": "smtp.qq.com",  # SMTP服务器地址
+    "smtp_port": 465,  # SSL端口
+
+    # 收件人
+    "receiver": "1293249206@qq.com",
+}
